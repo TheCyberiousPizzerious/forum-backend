1a7a0592d0aea90f7963275b4a2c190d00dff7f2
CyberPizza <your@example.com>
2024-04-17 22:16:52 +0200

    added trait to make easyJSON for Message structs

--- src/models/message_model.rs
+++ src/models/message_model.rs
@@ -9,3 +9,28 @@ pub struct MessageMessage {
 pub struct ErrorMessage {
     pub error: String,
 }
+
+/// traits for the different structs defined in message_models.rs
+pub trait MessageTraits {
+    /// function that takes a &str argument and returns an instance of a message struct
+    /// ```no_run
+    /// let message = MessageMessage::new_from("something that is in a message");
+    /// ```
+    fn new_from(str: String) -> Self;
+}
+
+impl MessageTraits for ErrorMessage {
+    fn new_from(str: String) -> Self {
+        Self {
+            error: str,
+        }
+    }
+}
+
+impl MessageTraits for MessageMessage {
+    fn new_from(str: String) -> Self {
+        Self {
+            message: str,
+        }
+    }
+}
