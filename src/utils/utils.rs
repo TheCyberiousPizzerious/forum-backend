71214b9fd008d3681e7ebf696d229f0fbb634d10
CyberPizza <your@example.com>
2024-04-17 22:14:26 +0200

    moved fn get_collection

--- src/utils/utils.rs
+++ src/utils/utils.rs
@@ -1,18 +1,8 @@
 use actix_web::{cookie::time::Date, get, rt::System, web::Json, HttpResponse, Responder};
-use mongodb::{Collection, Client, bson::{Bson, to_bson, Document, DateTime}};
-use serde::{Deserialize, Serialize};
-use serde_json::{to_string, Error};
+use mongodb::{Collection, Client, bson::{Bson, to_bson, Document, DateTime, ser::Error as BsonError}};
+use serde::Serialize;
+use serde_json::to_string;
 use std::time::SystemTime;
-
-/// Returns the selected Collection in the selected Database
-/// ```no_run
-/// let collection = get_collection(client_instance, database, collection);
-/// collection.insert_one("blahblahblah");
-/// ```
-pub fn get_collection(client: Client, database: String, collection: String) -> Collection<Document> { 
-    let db = client.database(&database);
-    db.collection(&collection)
-}
 
 /// Test the availability of the server
 #[get("/ping_server")]
@@ -33,17 +23,21 @@ pub async fn is_api_reachable() -> impl Responder {
 /// println!("result: {}", to_json(instance));
 /// // result {"name":"xx_epigamer_xx"}
 /// ```
-pub fn to_json<T: Serialize>(data: T) -> Result<String, Error> {
+pub fn to_json<T: Serialize>(data: T) -> Result<String, serde_json::Error> {
     match to_string(&data) {
         Ok(val) => Ok(val),
         Err(e) => Err(e),
     }
 }
 
+/// Returns a mongodb bson DateTime instance that is set to the current time
+/// keep in mind that this gives milli second precision
+/// ```no_run
+/// let now = bson_now();
+/// ```
 pub fn bson_now() -> DateTime {
     let now: SystemTime = SystemTime::now();
-    let bson_now: DateTime = DateTime::from(now);
-    bson_now
+    DateTime::from(now)
 }
 
 // Wip
