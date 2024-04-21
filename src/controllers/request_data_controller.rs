a369b91e3338d099761b76fffb703e7f8a456866
CyberPizza <your@example.com>
2024-04-21 21:44:08 +0200

    User with Uuid requester made, not tested

--- src/controllers/request_data_controller.rs
+++ src/controllers/request_data_controller.rs
@@ -1,17 +1,16 @@
-use actix_web::{get, web, HttpResponse};
-use uuid::Uuid;
-use std::str::FromStr;
+use crate::models::user_model::User;
+
+use actix_web::{
+    get, web, HttpResponse,
+    web::Data};
+use mongodb::{bson::doc, options::FindOneOptions, Client};
+use std::sync::Arc;
 
 #[get("/requestUserid/{id}")]
-pub async fn search_uuid(path: web::Path<String>) -> HttpResponse {
-
-    let requested_uuid = match Uuid::from_str(&path.into_inner()) {
-        Ok(val) => val,
-        Err(e) => {
-            eprintln!("Failed to parse requested id: {}", e);
-            Uuid::nil()
-        },
-    };
-
-    HttpResponse::ImATeapot().body("")
+pub async fn search_uuid(client: Data<Arc<Client>>, user_id: web::Path<User>) -> HttpResponse {
+    let uuid_string = user_id.user_id.to_string();
+    let filter = doc! { "user_id": uuid_string };
+    let find_options = FindOneOptions::builder().build();
+    let result = client.database("userStorage").collection::<User>("users").find_one(filter, find_options).await;
+    HttpResponse::ImATeapot().json(result.unwrap())
 }
--- src/utils/utils.rs
+++ src/utils/utils.rs
@@ -1,6 +1,6 @@
-use actix_web::{cookie::time::Date, get, rt::System, web::Json, HttpResponse, Responder};
+use actix_web::{cookie::time::Date, get, post, rt::System, web::Json, HttpRequest, HttpResponse, Responder};
 use mongodb::{Collection, Client, bson::{Bson, to_bson, Document, DateTime, ser::Error as BsonError}};
-use serde::Serialize;
+use serde::{Deserialize, Serialize};
 use serde_json::to_string;
 use std::time::SystemTime;
 
@@ -44,3 +44,42 @@ pub fn bson_now() -> DateTime {
 //pub fn from_json<T: Deserialize>(json: T) -> Result<T, Error> {
 //    Ok(T)
 //}
+
+/// # Get all user data
+/// retuns a lot of information from the different headers
+#[get("/grab-info")]
+pub async fn grab_info(info: HttpRequest) -> HttpResponse {
+    println!("head:         {:?}", info.head());
+    println!("uri:          {:?}", info.uri());
+    println!("method:       {:?}", info.method());
+    println!("version:      {:?}", info.version());
+    println!("headers:      {:?}", info.headers());
+    println!("path:         {:?}", info.path());
+    println!("query_str:    {:?}", info.query_string());
+    println!("match info:   {:?}", info.match_info());
+    println!("match pattrn: {:?}", info.match_pattern());
+    println!("match name:   {:?}", info.match_name());
+    //println!("conn_data:    {:?}", info.conn_data());
+    println!("conn info:    {:?}", info.connection_info());
+    if let Some(ip_string) = info.connection_info().peer_addr() {
+        println!("{ip_string}");
+    };
+    //connection_info
+    //realip_remote_addr
+    //peer_addr
+    //host
+    //scheme
+    HttpResponse::Ok().body("ok!")
+}
+
+#[derive(Deserialize, Debug)]
+struct Data {
+    data: String,
+}
+
+#[post("/send-data")]
+pub async fn send_data(thing: Json<Data>, req: HttpRequest) -> HttpResponse {
+    println!("call to {}", req.uri());
+    println!("got some data: {}", thing.data);
+    HttpResponse::Ok().body("ok!")
+}
