127479f74cbaee3763efdab1afb45320aad58a9e
TheCyberiousPizzerious <Trygve@harmaele.no>
2024-05-07 14:00:23 +0200

    something data handeler

--- src/controllers/request_data_controller.rs
+++ src/controllers/request_data_controller.rs
@@ -5,6 +5,8 @@ use actix_web::{
     web::Data};
 use mongodb::{bson::doc, options::FindOneOptions, Client};
 use std::sync::Arc;
+use uuid::Uuid;
+use std::str::FromStr;
 
 #[get("/requestUserid/{id}")]
 pub async fn search_uuid(path: web::Path<String>) -> HttpResponse {
@@ -19,3 +21,11 @@ pub async fn search_uuid(path: web::Path<String>) -> HttpResponse {
 
     HttpResponse::ImATeapot().body("")
 }
+
+/*
+pub async fn search_uuid(client: Data<Arc<Client>>, user_id: web::Path<User>) -> HttpResponse {
+    let uuid_string = user_id.user_id.to_string();
+    let filter = doc! { "user_id": uuid_string };
+    let find_options = FindOneOptions::builder().build();
+    let result = client.database("userStorage").collection::<User>("users").find_one(filter, find_options).await;
+*/
